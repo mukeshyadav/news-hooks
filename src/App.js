0c6d5bbc1f54ff7e9ec20fadade80aef19aed4b4
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-31 20:21:53 +0530

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     setLoading(true);
     try {
       const response = await fetch(
-        `http://hn.algolia.com/api/v1/searchquery=${query}`
+        `http://hn.algolia.com/api/v1/search?query=${query}`
       );
       let responseData = await response.json();
       setNewsResults(responseData.hits);
@@ -35,32 +35,52 @@ function App() {
     searchInputRef.current.focus();
   };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-indigo-200 shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="React Logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-blue-900 font-thin font-bold">Hooks News</h1>
+      <form onSubmit={handleSubmit} className="mb-2">
         <input
           type="search"
           value={query}
           onChange={event => setQuery(event.target.value)}
           ref={searchInputRef}
+          className="border p-1 rounded"
         />
-        <button type="submit">Seach</button>
-        <button type="button" onClick={handleClearSearch}>
+        <button type="submit" className="bg-black rounded m-1 p-2 text-white ">
+          Seach
+        </button>
+        <button
+          type="button"
+          className="bg-teal-900 rounded m-1 p-2 text-white "
+          onClick={handleClearSearch}
+        >
           Clear
         </button>
       </form>
-      <ul>
-        {loading ? (
-          <p>Loading Results...</p>
-        ) : (
-          newsResults.map(news => (
+
+      {loading ? (
+        <p className="font-bold text-yellow-600">Loading Results...</p>
+      ) : (
+        <ul className="list-reset leading-normal">
+          {newsResults.map(news => (
             <li key={news.objectID}>
-              <a href={news.url}>{news.title}</a>
+              <a
+                href={news.url}
+                className="text-indigo-600 hover:text-indigo-900"
+              >
+                {news.title}
+              </a>
             </li>
-          ))
-        )}
-      </ul>
-      {error && <div>{error.message}</div>}
-    </>
+          ))}
+        </ul>
+      )}
+
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
 
