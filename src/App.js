71841007d25e55175dec10146e5e7fe940f9bce9
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-31 19:59:13 +0530

    adding loading status using useEffect

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ function App() {
   const [newsResults, setNewsResults] = useState([]);
   const [query, setQuery] = useState("react hooks");
   const searchInputRef = useRef();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getNewsData();
   }, []);
 
   const getNewsData = async () => {
+    setLoading(true);
     const response = await fetch(
       `http://hn.algolia.com/api/v1/search?query=${query}`
     );
     let responseData = await response.json();
     setNewsResults(responseData.hits);
+    setLoading(false);
   };
 
   const handleSubmit = event => {
@@ -41,11 +44,15 @@ function App() {
         </button>
       </form>
       <ul>
-        {newsResults.map(news => (
-          <li key={news.objectID}>
-            <a href={news.url}>{news.title}</a>
-          </li>
-        ))}
+        {loading ? (
+          <p>Loading Results...</p>
+        ) : (
+          newsResults.map(news => (
+            <li key={news.objectID}>
+              <a href={news.url}>{news.title}</a>
+            </li>
+          ))
+        )}
       </ul>
     </>
   );
