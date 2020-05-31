6d1757733ceafc4f8e61512ddd8414c93c9d4589
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-31 19:41:15 +0530

    adding filter to search records

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [newsResults, setNewsResults] = useState([]);
+  const [query, setQuery] = useState("reacthooks");
+
   useEffect(() => {
     getNewsData();
-  }, []);
+  }, [query]);
 
   const getNewsData = async () => {
     const response = await fetch(
-      "http://hn.algolia.com/api/v1/search?query=reacthooks"
+      `http://hn.algolia.com/api/v1/search?query=${query}`
     );
     let responseData = await response.json();
     setNewsResults(responseData.hits);
   };
   return (
-    <ul>
-      {newsResults.map(news => (
-        <li key={news.objectID}>
-          <a href={news.url}>{news.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <input type="search" onChange={event => setQuery(event.target.value)} />
+      <ul>
+        {newsResults.map(news => (
+          <li key={news.objectID}>
+            <a href={news.url}>{news.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
