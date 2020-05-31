243bd84a5481adf5373471bb4efac2e6b85c6af5
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-31 19:35:35 +0530

    API call using useEffect, async/await

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">News App</div>;
+  const [newsResults, setNewsResults] = useState([]);
+  useEffect(() => {
+    getNewsData();
+  }, []);
+
+  const getNewsData = async () => {
+    const response = await fetch(
+      "http://hn.algolia.com/api/v1/search?query=reacthooks"
+    );
+    let responseData = await response.json();
+    setNewsResults(responseData.hits);
+  };
+  return (
+    <ul>
+      {newsResults.map(news => (
+        <li key={news.objectID}>
+          <a href={news.url}>{news.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
